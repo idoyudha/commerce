3dd7e0fec2059823243fc52b0bde1fae4f90362b
idoyudha <idowidya.yudhatama@gmail.com>
2021-01-29 22:25:56 +0700

    Update for models and admin

--- auctions/models.py
+++ auctions/models.py
@@ -6,13 +6,28 @@ class User(AbstractUser):
     pass
 
 # All of listing product
-class AuctionListings(models.Model):
-    pass
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=16, null=True)
+    category = models.CharField(max_length=16, null=True)
+    description = models.CharField(max_length=64, null=True)
+    price = models.FloatField(null=True)
+    image = models.ImageField(null=True)
 
-class Bids(models.Model):
-    pass 
+    def __str__(self):
+        return f"{self.title} - {self.category}"
+
+
+class Bid(models.Model):
+    bid = models.FloatField(null=True)
+
+    def __str__(self):
+        return f"{self.price}"
+
 
 class Comment(models.Model):
-    pass 
+    comment = models.CharField(max_length=64, null=True)
+
+    def __str__(self):
+        return f"{self.comment}"
 
 
