e457e25b9eeecbeafb407b2af7498f8348e54fbf
idoyudha <idowidya.yudhatama@gmail.com>
2021-02-03 13:34:04 +0700

    Comment form

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.db.models import fields
 from django.forms import ModelForm
 from django.forms.fields import ChoiceField
 from django.forms.widgets import TextInput, Textarea
@@ -11,7 +12,6 @@ class ListingForm(ModelForm):
         model = AuctionListing
         # we can use exclude or __all__ in this field below
         exclude = ('user',)
-        
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-control', 'id': 'title'}),
@@ -20,14 +20,18 @@ class ListingForm(ModelForm):
             'imageURL': forms.URLInput(attrs={'class': 'form-control', 'id': 'image'})
         }
 
-class BidForm(forms.Form):
-    bid = forms.IntegerField(widget = forms.NumberInput)
-    bid.widget.attrs.update({'class': 'form-control'})
-    # Widget
-    bid.widget.attrs.update({'class': 'form-control', 'id': 'bid'})
+class BidForm(ModelForm):
+    class Meta(object):
+        model = Bid
+        fields = ['bid']
+        widgets = {
+            'bid': forms.NumberInput(attrs={'class': 'form-control', 'id': 'bid'})
+        }
 
-class CommentForm(forms.Form):
-    comment = forms.CharField(max_length=64, required=False, widget = forms.Textarea)
-    comment.widget.attrs.update({'class': 'form-control'})
-    # Widget
-    comment.widget.attrs.update({'class': 'form-control', 'id': 'comment'})
+class CommentForm(ModelForm):
+    class Meta(object):
+        model = Comment
+        fields = ['comment_user']
+        widgets = {
+            'comment_user': forms.Textarea(attrs={'class': 'form-control', 'id': 'comment_user', 'rows': 2 })
+        }
--- auctions/models.py
+++ auctions/models.py
@@ -22,7 +22,6 @@ class AuctionListing(models.Model):
     description = models.CharField(max_length=1000, null=True, blank=True)
     price = models.IntegerField(null=True, blank=True)
     imageURL = models.URLField(null=True, blank=True)
-    
     def __str__(self):
         return f"{self.title} - {self.category}"
 
@@ -38,7 +37,8 @@ class Bid(models.Model):
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comment")
     title = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="title_comment")
-    comment = models.CharField(max_length=64, null=True)
+    comment_user = models.CharField(max_length=64, null=True)
+    time = models.DateTimeField(auto_now=True)
     def __str__(self):
         return f"{self.user}"
 
